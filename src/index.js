5cf02a79a0a99d3f1d7fe81f4a756510b3e0a935
sukhiboi <40167376+sukhiboi@users.noreply.github.com>
2020-09-05 21:54:39 +0530

    created raw react app with create-react-app

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <span> Twitter Clone</span>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
